d4b08d80277b9d29626521ac25a10f942ef4a6d7
espiler <monkeysrawesome@gmail.com>
2014-10-29 16:35:10 -0700

    add functional-shared queue and stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,36 @@
 var makeQueue = function(){
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var queue = {};
+  queue._storage = {};
+  queue._size = 0;
+  queue._newest= 0;
+  queue._oldest= 0;
+  extend(queue, queueMethods);
+
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(val){
+    this._storage[this._newest] = val;
+    this._size++;
+    this._newest++;
+  },
+  dequeue: function(){
+    if (this._size > 0) {
+      var result = this._storage[this._oldest];
+      delete this._storage[this._oldest];
+      this._oldest++;
+      this._size--;
+      return result;
+    }
+  },
+  size: function(){
+    return this._size;
+  }
+};
 
-
-
+var extend = function(obj1, obj2) {
+  for (var key in obj2) {
+    obj1[key] = obj2[key];
+  }
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+  stack._storage = {};
+  stack._size = 0;
+  extend(stack, stackMethods);
+
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(val) {
+    this._storage[this._size] = val;
+    this._size++;
+  },
+  pop: function(){
+    if (this._size > 0) {
+      var result = this._storage[this._size-1];
+      delete this._storage[this._size-1];
+      this._size--;
+      return result;
+    }
+  },
+  size: function(){
+    return this._size;
+  }
+};
 
 
+var extend = function(obj1, obj2) {
+  for (var key in obj2) {
+    obj1[key] = obj2[key];
+  }
+};
