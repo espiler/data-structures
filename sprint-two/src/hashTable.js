4a28cd182080009fbf6d9737f1607fca9b35fc69
espiler <monkeysrawesome@gmail.com>
2014-10-30 16:23:22 -0700

    Add hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,19 +4,32 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  if(!this._storage[index]){
+    this._storage[index] = [];
+  }
+  this._storage[index].push({ key: k, val: v});
 };
 
 HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  for(var i = 0; i < this._storage[index].length; i++){
+    if (this._storage[index][i] === null) {
+      return null;
+    } else if(this._storage[index][i].key === k) {
+      return this._storage[index][i].val;
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k){
-
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  for(var i = 0; i < this._storage[index].length; i++){
+    if(this._storage[index][i].key === k){
+      this._storage[index][i] = null;
+    }
+  }
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -29,7 +29,6 @@ treeMethods.contains = function(target){
   return result;
 };
 
-
 var extend = function(obj1, obj2) {
   for (var key in obj2) {
     obj1[key] = obj2[key];
