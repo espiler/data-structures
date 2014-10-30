ec48f58fff8c26844a858345b7bb4a9499376632
espiler <monkeysrawesome@gmail.com>
2014-10-30 10:30:21 -0700

    add linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,28 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    linkedListNode = makeNode(value);
+    if(list.tail !== null){
+      list.tail.next = linkedListNode;
+    }
+    if(list.head === null){
+      list.head = linkedListNode;
+    }
+    list.tail = linkedListNode;
   };
 
   list.removeHead = function(){
+    var result = list.head.value;
+    list.head = list.head.next;
+    return result;
   };
 
   list.contains = function(target){
+    for(var key in list){
+      if(list[key].value === target )
+        return true;
+    }
+    return false;
   };
 
   return list;
@@ -17,7 +33,6 @@ var makeLinkedList = function(){
 
 var makeNode = function(value){
   var node = {};
-
   node.value = value;
   node.next = null;
 
