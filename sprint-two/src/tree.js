e6a92e02f6cd3d54abf740efc450ce308118fd5c
espiler <monkeysrawesome@gmail.com>
2014-10-30 11:27:17 -0700

    add tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,21 +1,39 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+  extend(newTree, treeMethods);
   return newTree;
 };
-
-
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var subTree = makeTree(value);
+  this.children.push(subTree);
 };
 
 treeMethods.contains = function(target){
+  var result = false;
+  if (this.value === target) {
+    return true;
+  } else {
+    if(this.children.length > 0){
+      for(var i = 0; i < this.children.length; i++){
+        result = this.children[i].contains(target);
+        if(result){ return result; }
+      }
+    }
+  }
+  return result;
+};
 
+
+var extend = function(obj1, obj2) {
+  for (var key in obj2) {
+    obj1[key] = obj2[key];
+  }
 };
 
 
