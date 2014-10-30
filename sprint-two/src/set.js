0cfefa0d99e32696b5689e56bcace8c47e5e11dd
espiler <monkeysrawesome@gmail.com>
2014-10-30 10:48:16 -0700

    add set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,26 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item){
+  for (var key in this._storage) {
+    if (key === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item){
+  delete this._storage[item];
 };
 
 /*
